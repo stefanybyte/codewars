7877d68496111d40c0ec3df6fc415de24c42bb09
stefanybyte <stefany.pinzon111@gmail.com>
2025-04-16 00:26:00 -0400

    added classy extensions 8kyu

---
+++ 8kyu/classy.extensions.js
@@ -0,0 +1,17 @@
+// The goal of this kata is to train the basic OOP concepts of inheritance and method overriding.
+
+// You will be preloaded with the Animal class, so you should only edit the Cat class.
+
+// Task
+// Your task is to complete the Cat class which extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+// The name attribute is accessible in the class with this.name.
+
+//MY SOLUTION
+
+class Cat extends Animal {
+    speak(){
+      return (`${this.name} meows.`);
+    }
+  }
+  
