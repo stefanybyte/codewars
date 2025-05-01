5975c99e4468ef7188f0766636d89d4f85a69cd3
stefanybyte <stefany.pinzon111@gmail.com>
2025-05-01 16:51:04 -0400

    added four new files to 8kyu

---
+++ 8kyu/evenorodd.js
@@ -0,0 +1,11 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//MY SOLUTION
+
+function evenOrOdd(num){
+    if(num % 2 == 0){
+        return "Even";
+    }else{
+        return "Odd";
+    }
+};
---
+++ 8kyu/typeofsum.js
@@ -0,0 +1,8 @@
+// Return the type of the sum of the two arguments
+
+//MY SOLUTION
+
+function typeOfSum(a, b){
+    let sum = a + b
+    return typeof(sum);
+}
