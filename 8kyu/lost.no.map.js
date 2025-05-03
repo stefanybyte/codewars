a396906aaf1f3f2941fd5162c58c461e95be8948
stefanybyte <stefany.pinzon111@gmail.com>
2025-05-02 22:58:41 -0400

    added four files to 8kyu

---
+++ 8kyu/lost.no.map.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+//MY SOLUTION 
+
+function maps(x){
+    return x.map(function(element){
+        return element * 2;
+    });
+}
---
+++ 8kyu/remove.exclamationmarks.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//MY SOLUTION
+
+function removeExclamationMarks(s){
+    return s.replace(/!/g, "");
+}
