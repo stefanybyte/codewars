6aa9715536a2f3e6c5837d982a4b52fc89d18632
stefanybyte <stefany.pinzon111@gmail.com>
2025-05-17 21:14:16 -0400

    aeiou.js file 8kyu

---
+++ 8kyu/aeiou.js
@@ -0,0 +1,20 @@
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
+
+//MY SOULTION
+
+function isVowel(a){
+    return a.map(code => {
+        switch(a){
+        case 97:  return 'a';
+        case 101: return 'e';
+        case 105: return 'i';
+        case 111: return 'o';
+        case 117: return 'u';
+        default:  return code;
+      }
+    });
+}
